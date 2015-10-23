1fd12a2c064bd7f27dc50d85fc2b024976733c6a
Gertjan Roke <gertjan_roke90@hotmail.com>
2015-10-23 09:30:50 +0200

    Update EntranceServiceProvider.php

--- src/EntranceServiceProvider.php
+++ src/EntranceServiceProvider.php
@@ -29,7 +29,8 @@ class EntranceServiceProvider extends ServiceProvider
         // this  for conig
                 $this->publishes([
                         __DIR__.'/config/entrance.php' => config_path('entrance.php'),
-                        __DIR__.'/migrations' => database_path('migrations'),
+                        __DIR__.'/database/migrations' => database_path('migrations'),
+                        __DIR__.'/database/seeds' => database_path('seeds'),
                         __DIR__.'/models' => app_path(),
                         __DIR__.'/views/emails' => base_path('resources/views/intothesource/entrance/emails'),
                         __DIR__.'/views/pages' => base_path('resources/views/intothesource/entrance/pages'),
